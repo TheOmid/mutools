1df8a52f26feec3efe509afa6f4deebcc96ac15e
Darius Barbano <darius.barbano@gmail.com>
2022-01-09 14:26:35 -0500

    wrapping read example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+mod midi;
+
 #[cfg(test)]
 mod tests {
+    use super::midi;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_example() {
+       midi::read_midi_input()
     }
 }
