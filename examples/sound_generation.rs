0bfd4b1fc67f22d46e2a053906879ca29f5bcbdb
Darius Barbano <darius.barbano@gmail.com>
2022-04-27 18:41:05 -0400

    start fixing playback example

--- examples/sound_generation.rs
+++ examples/sound_generation.rs
@@ -1,14 +1,14 @@
 
-use mutools::sound::{MonoSignal, MonoFrame};
+use mutools::sound::{SterioSignal, SterioFrame};
 use dasp_signal::{self as signal, Signal};
+use dasp_ring_buffer as ring_buffer;
 use rodio::{Decoder, OutputStream, source::Source};
 
 fn generate_sine_sound() {
-
     let sine_wave = signal::rate(4.0).const_hz(1.0).sine();
+    let interleaved_samples = sine_wave.into_interleaved_samples();
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    stream_handle.play_raw(sine_wave);
-
+    stream_handle.play_raw(interleaved_samples.into_iter());
 }
 
 fn main() {
