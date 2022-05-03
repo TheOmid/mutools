0a2251a95ac58a46622c05b3a95139b0ee5ecb83
Darius Barbano <darius.barbano@gmail.com>
2022-05-03 15:18:18 -0400

    probably buggy triangle wavE

--- examples/sound_generation.rs
+++ examples/sound_generation.rs
@@ -1,20 +1,24 @@
 use dasp_ring_buffer as ring_buffer;
 use dasp_signal::{self as signal, Signal};
-use mutools::sound::{Sound, SterioSignal, SineWaveGenerator, SquareWaveGenerator};
+use mutools::sound::{Sound, SterioSignal, SineWaveGenerator, SquareWaveGenerator, TriangleWaveGenerator};
 use rodio::{source::Source, Decoder, OutputStream};
 
 fn generate_sine_sound() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     let mut sine_wave = Sound::new();
-    sine_wave.append_signal(SterioSignal::from_generator(SineWaveGenerator::new(220.0, 0.1, 44800), 44800*3));
+    sine_wave.append_signal(SterioSignal::from_generator(SineWaveGenerator::new(220.0, 0.3, 44800), 44800*3));
     stream_handle.play_raw(sine_wave.into_iter());
     std::thread::sleep(std::time::Duration::from_secs(3));
 
-
     let mut square_wave = Sound::new();
-    square_wave.append_signal(SterioSignal::from_generator(SquareWaveGenerator::new(220.0, 0.1, 32, 44800), 44800*3));
+    square_wave.append_signal(SterioSignal::from_generator(SquareWaveGenerator::new(220.0, 0.05, 32, 44800), 44800*3));
     stream_handle.play_raw(square_wave.into_iter());
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    let mut triangle_wave = Sound::new();
+    triangle_wave.append_signal(SterioSignal::from_generator(TriangleWaveGenerator::new(220.0, 0.1, 14, 44800), 44800*3));
+    stream_handle.play_raw(triangle_wave.into_iter());
     std::thread::sleep(std::time::Duration::from_secs(3));
 }
 
--- src/sound/generator.rs
+++ src/sound/generator.rs
@@ -71,3 +71,40 @@ impl SignalGenerator for SquareWaveGenerator {
         SterioFrame::from_vals(val, val)
     }
 }
+
+pub struct TriangleWaveGenerator {
+    frequency: f32,
+    amplitude: f32,
+    num_components: i32,
+    sample_rate: i32,
+}
+
+impl TriangleWaveGenerator {
+    pub fn new(frequency: f32, amplitude: f32, num_components: i32, sample_rate: i32) -> Self {
+        Self {
+            frequency,
+            amplitude,
+            num_components,
+            sample_rate
+        }
+    }
+
+    pub fn compute_frame(&self, idx: usize) -> f32 {
+        let sample_rate = self.sample_rate as f32;
+        let i = idx as f32;
+        let mut res = 0.0;
+        for n in 0..self.num_components {
+            let b = (n + 1) as f32;
+            let t = 2.0 * std::f32::consts::PI / sample_rate * self.frequency;
+            res += (1.0 / std::f32::consts::PI) * (1.0 / b) * (b * t * i).sin()
+        }
+        self.amplitude * (1.0/2.0 - res)
+    }
+}
+
+impl SignalGenerator for TriangleWaveGenerator {
+    fn generate_frame(&self, idx: usize) -> SterioFrame {
+        let val = self.compute_frame(idx);
+        SterioFrame::from_vals(val, val)
+    }
+}
