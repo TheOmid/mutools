f84fd2720564f4564271790a89b964f356b35833
axtyax <darius.barbano@gmail.com>
2022-05-23 23:08:27 -0400

    trying to call rpc from tauri

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -4,7 +4,25 @@
 )]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+
+    tauri::async_runtime::spawn(async move {
+	let rpc_client = {
+	    let res = mutools_rpc::client::MutoolsRpcClient::new(5000);
+	    match res {
+		Ok(client) => client,
+		_ => panic!("Mutools Client could not make contact with server!"),
+	    }
+	};
+	
+	let nd = rpc_client.get_client().get_num_project_descriptors(mutools_rpc::get_current_context()).await;
+	match nd {
+	    Ok(n) => println!("Got: {}", n),
+	    Err(_) => println!("Rpc returned an error"),
+	}
+    });
+    
+    
+    tauri::Builder::default()
+	.run(tauri::generate_context!())
+	.expect("error while running tauri application");
 }
--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,3 +1,7 @@
 pub mod client;
 pub mod interface;
 pub mod server;
+
+pub fn get_current_context() -> tarpc::context::Context {
+    tarpc::context::current()
+}
