2542962cea2fffdd33d3497d47fac668a339dd2f
axtyax <darius.barbano@gmail.com>
2022-06-18 21:50:21 -0400

    ref

--- src/project.rs
+++ src/project.rs
@@ -1,38 +1,47 @@
-use std::io::Write;
+use crate::*;
 
 // Core project type
-// Contains metadata like the project data's location, and manages loading/saving of project data.
+// Contains metadata like the project data file's location, and manages loading/saving of project data.
 pub struct Project {
     project_data: ProjectData,
+    data_file_path: std::string::String,
 }
 
-// Raw serializable type representing buffers, metadata, etc, contained in a project
+// Raw serializable project type representing the set of sounds in a project, and any project metadata.
 #[derive(serde::Serialize, serde::Deserialize)]
-pub struct ProjectData {}
+pub struct ProjectData {
+    sound_buffers: std::collections::HashSet<i32>,
+}
 
 impl ProjectData {
-    pub fn load_from_file(file: std::fs::File) -> Result<ProjectData, crate::errors::DataError> {
+    pub fn load_from_file(file: std::fs::File) -> Result<ProjectData, errors::DataError> {
         let bson_document_res = bson::Document::from_reader(file);
         match bson_document_res {
             Ok(bson_data) => {
                 match bson::from_bson::<ProjectData>(bson::Bson::Document(bson_data)) {
-                    Err(_) => Err(crate::errors::DataError::new()),
+                    Err(_) => Err(errors::DataError::new()),
                     Ok(data) => Ok(data),
                 }
             }
-            _ => Err(crate::errors::DataError::new()),
+            _ => Err(errors::DataError::new()),
         }
     }
 
-    pub fn save_to_file(self, file: std::fs::File) -> Result<(), crate::errors::DataError> {
+    pub fn save_to_file(self, file: std::fs::File) -> Result<(), errors::DataError> {
         let bson_res = bson::to_bson(&self);
         match bson_res {
             Ok(bson::Bson::Document(bson_data)) => match bson_data.to_writer(file) {
                 Ok(_) => Ok(()),
-                _ => Err(crate::errors::DataError::new()),
+                _ => Err(errors::DataError::new()),
             },
-            _ => Err(crate::errors::DataError::new()),
+            _ => Err(errors::DataError::new()),
         }
+    }
+
+    // Hash all data in this object.
+    //  -> Allows testing objects for equality.
+    pub fn get_data_hash(&self) -> i32 {
+        0
     }
 }
 
@@ -40,4 +49,16 @@ impl Project {
     pub fn get_project_data(&self) -> &ProjectData {
         &self.project_data
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn load_and_save_project() -> () {
+        ()
+    }
+
 }
